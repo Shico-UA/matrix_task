e2eeb67fbf3135b0e63dcc8755df36a7dc82a9bc
s_shishka@ukr.net <you@example.com>
2016-07-14 18:10:57 +0300

    task completed for 2-dimensional matrix

--- matrix.py
+++ matrix.py
@@ -1,9 +1,10 @@
 import random
 
+
 # Create and return matrix raw
 def create_matrix_raw():
     raw = []
-    [raw.append(random.randint(0, 9)) for num in range(10)]
+    [raw.append(random.randint(-9, 9)) for num in range(10)]
     return raw
 
 
@@ -12,13 +13,15 @@ def create_matrix():
     matrix = [create_matrix_raw() for raw in range(10)]
     return matrix, "Matrix length is: %d" % len(matrix)
 
+
 # Here we get a tuple with 2 elements(matrix & matrix length)
 matrix = create_matrix()
 
 
 # Display matrix in console
 def display_matrix(matrix):
-    for raw in matrix[0]: print raw
+    for raw in matrix[0]:
+        print raw
     print "*" * 80
     print matrix[1]
     print "*" * 80
@@ -26,19 +29,22 @@ def display_matrix(matrix):
 print "Displaying our matrix"
 print "*" * 80
 display_matrix(matrix)
-        
+
 
 # Find random matrix raw
 random_raw_num = random.randint(0, len(matrix))
+
+
 def find_raw(matrix, random_raw_num):
     return matrix[0][random_raw_num]
 
 raw = find_raw(matrix, random_raw_num)
-print "Random raw from our matrix: {}".format(raw)
-print "*" * 80
+
 
 # Find random matrix column
 random_col_num = random.randint(0, len(matrix))
+
+
 def find_col(matrix, random_col_num):
     column = []
     for item in matrix[0]:
@@ -46,8 +52,6 @@ def find_col(matrix, random_col_num):
     return column
 
 column = find_col(matrix, random_col_num)
-print "Random column from our matrix: {}".format(column)
-print "*" * 80
 
 
 # Function that takes two lists and multiply them
@@ -62,8 +66,32 @@ def lists_multiply(list_1, list_2):
     return mult_result
 
 mult_result = lists_multiply(raw, column)
-print """The results of the multiplication of random raw to random column:
-{}""".format(mult_result)
-print "*" * 80
 
 
+# Function that calculate intersection of raw and column
+def intersect(matrix):
+    results = []
+    counter = 0
+    for i in range(0, len(matrix[0])):
+        for j in range(0, len(matrix[0])):
+            i_raw = find_raw(matrix, i)
+            j_column = find_col(matrix, j)
+            divider = i_raw[j]
+            results.append(lists_multiply(i_raw, j_column))
+            try:
+                results[counter] = results[counter] / divider
+            except ArithmeticError:
+                results[counter] = 0
+            counter += 1
+    return results
+
+
+intersections = intersect(matrix)
+
+
+# Find the three minimum values
+def find_minimum(intersections):
+    return [min(intersections) for i in range(3)]
+
+print "Three minim values of perpendicular intersection in the matrix are: "
+print find_minimum(intersections)
